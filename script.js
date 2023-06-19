877ec26a45b5997fa440a8324abedbf96c59b211
Gullied Mohamed <gulleidmohamed93@gmail.com>
2023-06-19 15:03:40 +0300

    Reverse words

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 // Good Luck 💪🏾
+function reverse(doc) {
+    const words = doc.split(" ");
+    const gmcc1 = words.reverse();
+    const rdoc = gmcc1.join(" ");
+    return rdoc;
+  }
+  const doc = "";
+ 
+  const reversed = reverse(doc);
+  console.log(reverse("fikrcamp bootcamp"));
+  console.log(reverse("hello there"));
+  console.log(reverse("welcome to my city "));
